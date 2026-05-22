b0ede5303231e865a1ffaf70bcdee58799c4258b
Lord Sprite <spiridonov-matvey98764@mail.ru>
2026-05-22 18:18:36 +0300

    banch

--- src/infrastructure/database/migrations/002_goals_and_levels.sql
+++
@@ -1,4 +0,0 @@
--- Добавляем колонку уровня опыта
-ALTER TABLE users ADD COLUMN experience_level VARCHAR(20) DEFAULT 'novice';
--- Добавляем колонку цели тренировок
-ALTER TABLE users ADD COLUMN fitness_goal VARCHAR(50) DEFAULT 'maintenance';
